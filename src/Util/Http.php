c8a350c1b2e03b725bb4594370160fb8541ecf38
webpajooh@ <webpajooh@gmail.com>
2020-05-22 23:17:31 +0430

    Create Http class

---
+++ src/Util/Http.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace TeleBot\Util;
+
+class Http
+{
+    public static function post($url, array $fields)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($fields));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        return json_decode($result);
+    }
+}
