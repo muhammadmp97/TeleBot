4740e8b1efd53bbd907f76e25cdfab00d4b9584b
webpajooh@ <webpajooh@gmail.com>
2020-05-24 15:25:22 +0430

    Update logger class

--- src/Util/Logger.php
+++ src/Util/Logger.php
@@ -6,6 +6,16 @@ class Logger
 {
     public static function log($message)
     {
-        // Log
+        $filePath = dirname($_SERVER['SCRIPT_FILENAME']) . '/log.txt';
+        $time = (new \DateTime())->format('Y-m-d H:i:s');
+        $message = "({$time}): {$message}";
+
+        if (file_exists($filePath)) {
+            $message .= "\n" . file_get_contents($filePath);
+        } else {
+            touch($filePath);
+        }
+
+        file_put_contents($filePath, $message);
     }
 }
